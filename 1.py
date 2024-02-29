50373acae3070e8e29e407efb27562b234d42670
ashwhi06 <ashe.white06@gmail.com>
2024-02-28 21:29:11 -0500

    Ex1 complete

--- 1.py
+++ 1.py
@@ -1,3 +1,6 @@
 # Write your solution for algorithm 1 below
 #1. Write an algorithm that takes in an unsorted numerical list as an argument and sorts the list (use the sort() method).
 
+lst = [5, 10, 4, 7, 6, 2] #unsorted list
+lst.sort() #sorts list in ascending order
+print(lst) #prints sorted list
