7a5edb805ecd21eaa66e5d42fb3267894cd9d2e8
ashwhi06 <ashe.white06@gmail.com>
2024-02-28 21:33:26 -0500

    Ex3 complete

--- 3.py
+++ 3.py
@@ -2,3 +2,6 @@
 #3. Write an algorithm that takes in an unsorted numerical list as an argument and creates a new sorted list in descending order (use the sorted() function).
 
 #The sorted() function sorts in ascending order, but it can sort in descending order by adding a reverse parameter with a boolean value of True.
+lst = [5, 10, 9, 7, 6, 12] #unsorted list
+sorted_lst = sorted(lst, reverse = True) # sort in descending order
+print(sorted_lst) #prints sorted list
