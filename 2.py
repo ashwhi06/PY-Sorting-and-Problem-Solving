cc3543775fadc078a0316ff0b1c1c0f5a27b1f4b
ashwhi06 <ashe.white06@gmail.com>
2024-02-28 21:31:32 -0500

    Ex2 complete

--- 2.py
+++ 2.py
@@ -1,2 +1,6 @@
 # Write your solution for algorithm 2 below
 #2. Write an algorithm that takes in an unsorted numerical list as an argument and creates a new sorted list (use the sorted() function).
+
+lst = [5, 10, 4, 7, 6, 2] #unsorted list
+sorted_lst = sorted(lst) # create sorted list
+print(sorted_lst) #prints sorted list
