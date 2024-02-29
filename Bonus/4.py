723aec7a14cb589355b5a444340fb0d8eb91a7fd
ashwhi06 <ashe.white06@gmail.com>
2024-02-28 21:35:43 -0500

    Ex 4 complete

--- Bonus/4.py
+++ Bonus/4.py
@@ -4,3 +4,7 @@
 # Sample input: 'I love software engineering'
 # Sample output: ['engineering', 'I', 'love', 'software']
 # Note that a key parameter will need to be used here in order for the sorting to be case-insensitive (sort in alphabetical order regardless of whether string is uppercase or lowercase)
+
+word_str = 'I love software engineering' #unsorted string sentence
+sorted_str = sorted(word_str.split(), key = str.lower) #sorts string in alphabetical order
+print(sorted_str) #prints sorted string
